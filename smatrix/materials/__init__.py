c3954be0b298f082f1faf81c988e569eaec83f7c
Alexandre Juneau Fecteau <alexandre.juneau.fecteau@usherbrooke.ca>
2018-05-16 13:58:34 -0400

    Added Au dielectric function

--- smatrix/materials/__init__.py
+++ smatrix/materials/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .diel_func import diel_vac, diel_SiC, diel_AlN, diel_SiO2
-from .N_func import N_vac, N_SiC, N_AlN, N_SiO2
+from .diel_func import diel_vac, diel_SiC, diel_AlN, diel_SiO2, diel_Au
+from .N_func import N_vac, N_SiC, N_AlN, N_SiO2, N_Au
 from .emt import diel_MG, N_MG
--- smatrix/materials/diel_func.py
+++ smatrix/materials/diel_func.py
@@ -66,13 +66,42 @@ def diel_SiO2(wvl):
     Interpolated from Palik et al.
     """
 
-    n_pth = "/".join(("data", "n_SiO2_Palik.dat"))
+    n_pth = "/".join(("data", "SiO2", "n_SiO2_Palik.dat"))
     rsc_n = pkg_resources.resource_string(pkg_name, n_pth)
     data_n = np.genfromtxt(BytesIO(rsc_n), delimiter=",")
     wvl_n_dat = 1e-6*data_n[:, 0]  # m
     n_dat = data_n[:, 1]
 
-    k_pth = "/".join(("data", "k_SiO2_Palik.dat"))
+    k_pth = "/".join(("data", "SiO2", "k_SiO2_Palik.dat"))
+    rsc_k = pkg_resources.resource_string(pkg_name, k_pth)
+    data_k = np.genfromtxt(BytesIO(rsc_k), delimiter=",")
+    wvl_k_dat = 1e-6*data_k[:, 0]  # m
+    k_dat = data_k[:, 1]
+
+    # Interpolate n&k with cubic spline
+    n_itp_func = interp1d(wvl_n_dat, n_dat, kind="cubic")
+    k_itp_func = interp1d(wvl_k_dat, k_dat, kind="cubic")
+
+    return (n_itp_func(wvl) + 1j*k_itp_func(wvl))**2
+
+
+#---------------------------------------------------------#
+# Au
+
+
+def diel_Au(wvl):
+    """
+    Dielectric function of Au
+    Interpolated from Olmon et al.
+    """
+
+    n_pth = "/".join(("data", "Au", "n_Au_Olmon.dat"))
+    rsc_n = pkg_resources.resource_string(pkg_name, n_pth)
+    data_n = np.genfromtxt(BytesIO(rsc_n), delimiter=",")
+    wvl_n_dat = 1e-6*data_n[:, 0]  # m
+    n_dat = data_n[:, 1]
+
+    k_pth = "/".join(("data", "Au", "k_Au_Olmon.dat"))
     rsc_k = pkg_resources.resource_string(pkg_name, k_pth)
     data_k = np.genfromtxt(BytesIO(rsc_k), delimiter=",")
     wvl_k_dat = 1e-6*data_k[:, 0]  # m
